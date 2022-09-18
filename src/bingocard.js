edc3d3eec98205c5c5f0ad346ef48ddbb1ef7929
pavemen3 <43461016+pavemen3@users.noreply.github.com>
2022-09-18 14:53:51 +0900

    js, tsに移植

---
+++ src/bingocard.js
@@ -0,0 +1,32 @@
+// jsに移植(deno環境で実行deno run xxxx.ts)
+// Fisher-Yatesアルゴリズム
+Array.prototype.shuffle = function() {
+  let i = this.length;
+  while(i) {
+    let j = Math.floor(Math.random() * i);
+    let t = this[--i];
+    this[i] = this[j];
+    this[j] = t;
+  }
+  return this;
+} 
+// 1から75までの値リストを用意
+// [200~https://qiita.com/takehilo/items/59cb8dc93d28ca252570
+const nums = Array.from(Array(75).keys(), x => x + 1);
+// シャッフル
+const numsShuffle = nums.shuffle();
+
+// カードを表示
+for(let y = 0; y < 5; y++) {
+  let line = "";
+  for(let x = 0; x < 5; x++) {
+    let i = y * 5 + x;
+    if(i === 12) {
+      line += "  *,";
+    } else {
+      line += numsShuffle[i] + ",";
+    }
+  }
+  console.log(line);
+}
+
